ad3bbe9fbc3666be69b7fb32711d0669bffab8d6
Isaiah Che <hunter66888@gmail.com>
2023-02-05 16:08:06 -0500

    Added cards and gameplay rules

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public enum TYPE { 
+        PREFIX,
+        SUFFIX,
+        ROOT
+    };
+    private static System.Random rng = new System.Random();
+    public string word;
+    public int points;
+    public TYPE type;
+    public TextMeshPro tmp;
+    public bool hasPlayed;
+    public int handIdx;
+    private GameMode gm;
+    private Transform originalPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = FindObjectOfType<GameMode>();
+        tmp.text = word;
+        points = rng.Next(1, 11);
+        originalPos = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (type == TYPE.ROOT)
+            return;
+
+        if(!hasPlayed)
+        {     
+            hasPlayed = true;
+            gm.availableSlots[handIdx] = true;
+            if (type == TYPE.PREFIX)
+            {
+                transform.position = gm.prePos.position;
+                gm.prefix = this;
+            }
+            else
+            {
+                transform.position = gm.suffPos.position;
+                gm.suffix = this;
+            }
+        }
+        else
+        {
+            transform.position = originalPos.position;
+            hasPlayed = false;
+            gm.availableSlots[handIdx] = false;
+        }
+    }
+
+    public void Discard()
+    {
+        transform.position = gm.discardPos.position;
+        gm.discardPile.Add(this);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/GameMode.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameMode : MonoBehaviour
+{
+    public List<string> prompts;
+    public List<Card> roots;
+    public Transform discardPos;
+    public List<Card> discardPile;
+    public List<Card> words;
+    public Transform[] hand;
+    public Card root;
+    public Card prefix;
+    public Card suffix;
+    public bool[] availableSlots;
+    public int score;
+    public TextMeshPro scoreBoard;
+    public TextMeshPro promptBoard;
+    private string currentPrompt;
+
+    public Transform rootPos;
+    public Transform prePos;
+    public Transform suffPos;
+
+    public void Start()
+    {
+        rootPos = root.transform;
+        prePos = prefix.transform;
+        suffPos = suffix.transform;
+        NewTurn();
+    }
+    public void NewTurn()
+    {
+        if (prompts.Count >= 1)
+        {
+            int promptIdx = Random.Range(0, prompts.Count - 1);
+            currentPrompt = prompts[promptIdx];
+            prompts.Remove(currentPrompt);
+            promptBoard.text = currentPrompt;
+
+            root = roots[Random.Range(0, roots.Count - 1)];
+            root.transform.position = rootPos.position;
+            root.gameObject.SetActive(true);
+            roots.Remove(root);
+
+            for (int i = 0; i < availableSlots.Length; i++)
+            {
+                if (words.Count == 0)
+                    return;
+                if (availableSlots[i])
+                {
+                    Card drawed = words[Random.Range(0, words.Count - 1)];
+                    drawed.gameObject.SetActive(true);
+                    drawed.handIdx = i;
+                    drawed.transform.position = hand[i].position;
+                    availableSlots[i] = false;
+                    words.Remove(drawed);
+                }
+            }
+        }
+    }
+
+    public void OnSubmit()
+    {
+        string word = "";
+        if (prefix != null)
+        {
+            score += prefix.points;
+            word += prefix.word;
+            prefix.Discard();
+        }
+        word += root.word;
+        score += root.points;
+        root.Discard();
+        if (suffix != null)
+        {
+            score += suffix.points;
+            word += suffix.word;
+            suffix.Discard();
+        }
+        scoreBoard.text = "" + score;
+        currentPrompt = currentPrompt.Replace("___", word);
+        promptBoard.text = currentPrompt;
+        Invoke("NewTurn", 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
